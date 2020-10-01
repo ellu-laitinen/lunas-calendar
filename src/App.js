5577c63e4bc8df4e5835630c2ddc2706c66fe928
ElluLaitinen <ellu.laitinen85@gmail.com>
2020-10-01 09:09:34 +0300

    add content to windows 3, 4, 17, 24

--- src/App.js
+++ src/App.js
@@ -5,25 +5,29 @@ import axios from 'axios';
 import Window from './Window/Window';
 import './App.css'
 
-/* const shuffle = a => {
+const shuffle = a => {
   for (let i = a.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [a[i], a[j]] = [a[j], a[i]];
 
   }
   return a;
-}; */
+};
 
 
 function App() {
   const [window, setWindow] = useState([])
 
 
+  useEffect(() => {
+    window.length && localStorage.setItem('calendar', JSON.stringify(window));
+  }, [window])
+
 
   useEffect(() => {
     axios.get("http://localhost:3001/windows").then((response) => {
       const windows = response.data
-      setWindow(windows)
+      setWindow(shuffle(windows))
       const calendar = localStorage.calendar
         ? JSON.parse(localStorage.calendar)
         : window;
@@ -31,11 +35,6 @@ function App() {
       setWindow(calendar);
     })
   }, [])
-
-  useEffect(() => {
-    window.length && localStorage.setItem('calendar', JSON.stringify(window));
-  }, [window])
-
 
 
   /*  useEffect(() => {
